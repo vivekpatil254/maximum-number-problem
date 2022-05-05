f2ccd9472a299b482634329ee521318d1d6eb346
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 <Vivekpatil254@gmail.com>
2022-05-06 01:11:25 +0530

    UC1 commit

---
+++ src/com/bridgelabz/TestMaxiumMain.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class TestMaxiumMain {
+
+    public static void main(String[] args) {
+	TestMaxiumMain object =new TestMaxiumMain();
+
+    }
+    public void compereTo(Integer object1, Integer object2, Integer object3)
+    {
+        if(object1>object2 && object1 > object3)
+            System.out.println("The "+object1+"is maximum number among three numbers.")
+        else if(object2>object1 && object2 > object3)
+            System.out.println("The "+object2+"is maximum number among three numbers."
+        else
+            System.out.println("The "+object3+"is maximum number among three numbers."
+
+
+    }
+}
